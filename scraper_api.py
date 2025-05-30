17b109161ce57ad7589dff0f838046a6ba6ceb27
lloydbrown4 <l.alexanderbrown@gmail.com>
2025-05-30 13:39:54 +0100

    Update scraper_api.py

--- scraper_api.py
+++ scraper_api.py
@@ -45,5 +45,7 @@ def get_contracts():
     return jsonify(contracts)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
