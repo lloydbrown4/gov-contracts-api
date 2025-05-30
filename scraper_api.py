c1f51ba91bd57024aad7120c9e36cefbbd9b0d2f
lloydbrown4 <l.alexanderbrown@gmail.com>
2025-05-30 12:25:28 +0100

    Update scraper_api.py

--- scraper_api.py
+++ scraper_api.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS (app)
 
 @app.route('/api/contracts')
 def get_contracts():
